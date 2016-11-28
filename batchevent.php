4d75538c3223b6633a9499689dfd073465564526
McFlip <gradydenton@yahoo.com>
2016-11-28 14:35:06 -0500

    season events done

--- batchevent.php
+++ batchevent.php
@@ -15,22 +15,61 @@ try {
 	echo "Connection failed: " . $e->getMessage();
 	die();
 }
-$today = date_create();
+$today = date_create(null, new DateTimeZone('UTC'));
 //seasonal
-$sql = "UPDATE `inventory`
-				SET `visible`=1
-				WHERE `sku`=(
-					SELECT `sku`
-					FROM `inventory`,`member`,`event`
-					WHERE `inventory`.`memberID`=`member`.`memberID`
-					  AND `member`.`memberID`=`event`.`memberID`
-					  AND `event`.`category`='season'
-						AND `event`.`start` LIKE '".date_format($today, 'Y-m-d')."%')";
+//Check if a season starts today and make matching patterns visible
+$sql = "SELECT `event`.`season`, `event`.`memberID`
+        FROM `event`,`inventory`
+        WHERE `event`.`memberID`=`inventory`.`memberID`
+        AND `event`.`category`='season'
+				AND `event`.`start` LIKE '".date_format($today, 'Y-m-d')."%'
+				AND `inventory`.`pattern` LIKE CONCAT('%',`event`.`season`,'%')";
 try {
-	$conn->exec($sql);
+	$pdo = $conn->query($sql);
+	$seasonal = $pdo->fetch();
 } catch(PDOException $e) {
-	echo "season statement failed: " . $e->getMessage();
+	echo "find seasonal event statement failed: " . $e->getMessage();
 	die();
 }
-echo "season statement succeded\n";
+if ($seasonal){
+  $sql = "UPDATE `inventory`
+				SET `visible`=1
+				WHERE `inventory`.`pattern` LIKE '%".$seasonal['season']."%'
+				AND `inventory`.`memberID`=".$seasonal['memberID'].";";
+	try {
+		$conn->exec($sql);
+	} catch(PDOException $e) {
+		echo "season make visible statement failed: " . $e->getMessage();
+		die();
+	}
+}
+echo "season make visible statement succeded\n";
+//Check if a season ends today and hide matching patterns
+$sql = "SELECT `event`.`season`, `event`.`memberID`
+        FROM `event`,`inventory`
+        WHERE `event`.`memberID`=`inventory`.`memberID`
+        AND `event`.`category`='season'
+				AND `event`.`end` LIKE '".date_format($today, 'Y-m-d')."%'
+				AND `inventory`.`pattern` LIKE CONCAT('%',`event`.`season`,'%')";
+try {
+	$pdo = $conn->query($sql);
+	$seasonal = $pdo->fetch();
+} catch(PDOException $e) {
+	echo "find seasonal event statement failed: " . $e->getMessage();
+	die();
+}
+if ($seasonal){
+  $sql = "UPDATE `inventory`
+				SET `visible`=0
+				WHERE `inventory`.`pattern` LIKE '%".$seasonal['season']."%'
+				AND `inventory`.`memberID`=".$seasonal['memberID'].";";
+	try {
+		$conn->exec($sql);
+	} catch(PDOException $e) {
+		echo "season make visible statement failed: " . $e->getMessage();
+		die();
+	}
+}
+echo "season hide statement succeded\n";
+$conn = null;
 ?>
